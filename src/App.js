e17ff79ee2e2238e86582ee6b3fb845f19be2070
myllys <saanteri.myllys@gmail.com>
2022-03-10 10:15:06 +0200

    muutos testi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
 
 export const buttonThemes = {
   blue: {
@@ -11,11 +10,33 @@ export const buttonThemes = {
     backgroundColor: 'black'
   },
 };
+const ThemeContext = React.createContext(buttonThemes.blue);
 
 function App() {
+  
   return (
-    <Header />
+    <ThemeContext.Provider value={buttonThemes.black}>
+      <Toolbar />
+    </ThemeContext.Provider>
   );
 }
 
+function Toolbar(props) {
+  return (
+    <div>
+      <HeaderButton />
+    </div>
+  );
+}
+function HeaderButton() {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <div>
+      <button style={{color: 'white',
+    backgroundColor: 'blue' }}>
+      Press me!
+    </button>
+    </div>
+  );
+}
 export default App;
